cbcf09e6c39220a3c634b04ff9a95b3daa6bb601
Rajnik <rajnik.rails@gmail.com>
2015-09-26 18:50:39 +0530

    added the device for authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://salty-savannah-9977.herokuapp.com/' }
 end
